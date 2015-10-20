3daea77f3cb84c26ac8d9d9cb65e231c796186c4
Michael Palmer <mecfiremike@gmail.com>
2015-10-20 13:17:43 -0400

    Update Computer Powershell Script

---
+++ Update-ADComputer.ps1
@@ -0,0 +1,6 @@
+﻿$ComputerList = Import-CSV "C:\Users\Michael.Palmer\Desktop\ComputerUpdate.csv"
+
+ForEach ($Computer in $ComputerList) {
+Get-ADComputer $Computer.Name | Set-ADComputer -Description $Computer.Description -Location $Computer.Location
+
+}
